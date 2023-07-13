65adcce15accd9bba9cb0151dbc9f1a45397b588
JKnight3517 <knightjknight@gmail.com>
2023-07-12 21:51:27 -0700

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Justin Knight on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
