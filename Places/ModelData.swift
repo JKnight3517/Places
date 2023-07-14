cdb1ffc8d8e3a447deb90f66a5be6312732501f8
JKnight3517 <knightjknight@gmail.com>
2023-07-13 18:26:44 -0700

    Set up modal for landmarks and create load function
    Load function should work to pull data from the mockData json file

---
+++ Places/ModelData.swift
@@ -0,0 +1,32 @@
+//
+//  ModelData.swift
+//  Places
+//
+//  Created by Justin Knight on 7/13/23.
+//
+
+import Foundation
+
+
+func load<T: Decodable>(_ fileName: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: fileName, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(fileName) in main bundle")
+                   }
+    do {
+        data = try Data(contentsOf: file)
+        
+    } catch {
+        fatalError("Couldn't load \(fileName) from main bundle: \(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(fileName) as \(T.self):\n\(error)")
+    }
+                   
+}
