cd3219f9237ccb08c9902cf39c2019ec2b901c80
JKnight3517 <knightjknight@gmail.com>
2023-08-14 23:51:37 -0700

    Add a favorite filter to our places list

--- Places/Views/LandmarkList.swift
+++ Places/Views/LandmarkList.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landMark: landmark)
-                } label: {
-                    LandmarkRow(landMark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                       
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landMark: landmark)
+                    } label: {
+                        LandmarkRow(landMark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
