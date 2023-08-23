4b26b07cbfc77faa534d94d5c1f6f82d11f97c57
JKnight3517 <knightjknight@gmail.com>
2023-08-23 11:28:35 -0700

    Connect both the categories and the list views into tab view

--- Places/Views/ContentView.swift
+++ Places/Views/ContentView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var tabSelection: Tab = .featured
+    
+    enum Tab {
+        case featured
+        case list
+    }
     var body: some View {
-        LandmarkList()
+        
+        TabView(selection: $tabSelection) {
+            CategoryHome()
+                .tabItem{
+                    Label("Featured", systemImage: "star")
+                    
+                }
+                .tag(Tab.featured)
+            LandmarkList()
+                .tabItem {
+                    Label("List", systemImage: "list.bullet")
+                }
+                .tag(Tab.list)
+                
+        }
     }
 }
 
