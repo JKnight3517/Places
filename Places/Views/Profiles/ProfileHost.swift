0d3a74d8ed395115bdde48b9eca13bb0c920a5d7
JKnight3517 <knightjknight@gmail.com>
2023-08-23 18:55:43 -0700

    Add a profile view and editor mode
    Still have to wire up the saving

--- Places/Views/Profiles/ProfileHost.swift
+++ Places/Views/Profiles/ProfileHost.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ProfileHost: View {
-    
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State private var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-                 ProfileSummary(profile: draftProfile)
+            
+            HStack {
+                HStack {
+                    Spacer()
+                    EditButton()
+                }
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                       ProfileSummary(profile: modelData.profile)
+                   } else {
+                       ProfileEditor(profile: $draftProfile)
+                   }
              }
              .padding()
     }
@@ -22,5 +35,6 @@ struct ProfileHost: View {
 struct ProfileHost_Previews: PreviewProvider {
     static var previews: some View {
         ProfileHost()
+            .environmentObject(ModelData())
     }
 }
