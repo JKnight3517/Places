49fbbc38f68fc2a9e6f1e8af7fc0aa8b552fe094
JKnight3517 <knightjknight@gmail.com>
2023-08-23 21:29:09 -0700

    Add a page control view and add it to the home screen

---
+++ Places/Views/PageView/PageControl.swift
@@ -0,0 +1,52 @@
+//
+//  PageControl.swift
+//  Places
+//
+//  Created by Justin Knight on 8/23/23.
+//
+
+import SwiftUI
+import UIKit
+
+
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+        control.addTarget(
+               context.coordinator,
+               action: #selector(Coordinator.updateCurrentPage(sender:)),
+               for: .valueChanged)
+        
+        return control
+    }
+    
+    
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+    
+    class Coordinator: NSObject {
+        var control: PageControl
+        
+        
+        init(_ control: PageControl) {
+            self.control = control
+        }
+        
+        
+        @objc func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
+    }
+}
+
+
